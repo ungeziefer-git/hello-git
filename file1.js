7c4c92a38d24e2fe6f39c326133faa9d3014bb24
ungeziefer.ubuntu <ungeziefer.ubuntu@gmail.com>
2018-10-08 11:11:07 +0900

    commit by feature1 for rebase test
    commit by feature1 for rebase test again

--- file1.js
+++ file1.js
@@ -2,3 +2,4 @@ console.log('file1 changed by feature1');
 console.log('file1 changed');
 console.log('file1 changed by master');
 console.log('file1 changed by feature1');
+console.log('file1 changed by feature1 again');
